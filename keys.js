edc287760f5e38bd35fdb005270e14a69dd19b26
kmgulledge <kmgulledge@gmail.com>
2017-07-30 16:39:12 -0400

    initial upload

---
+++ keys.js
@@ -0,0 +1,13 @@
+// console.log('keys.js loaded');
+
+exports.twitterKeys = {
+        consumer_key: '',
+        consumer_secret: '',
+        access_token_key: '',
+        access_token_secret: '',
+    } // end twitterKeys{}
+
+exports.spotifyKeys = {
+        id: "",
+        secret: ""
+    } // end spotifyKeys{}
