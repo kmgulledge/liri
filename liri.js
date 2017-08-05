1a7d97efb799e8e8934244fd776f827c5d3434e7
kmgulledge <kmgulledge@gmail.com>
2017-08-05 09:38:42 -0400

    hope this works

--- liri.js
+++ liri.js
@@ -13,18 +13,24 @@ var request = require("request");
 var action = process.argv[2];
 var value = process.argv[3];
 var userTwitter = "kmguncc";
-var client = new twitter(keys.twitterKeys);
+var feed = new twitter(keys.twitterKeys);
 var spotify = new Spotify(keys.spotifyKeys);
 
 
 switch (action) {
     case "my-tweets":
-        retrieveTweets();
+    case "twitter":
+    case "getTweets":
+        myTweets();
         break;
     case "spotify-this-song":
+    case "spotify":
+    case "spotify-this":
         getSong(value);
         break;
     case "movie-this":
+    case "movieThis":
+    case "movie":
         getMovie(value);
         break;
     case "do-what-it-says":
@@ -32,9 +38,15 @@ switch (action) {
         break;
 }
 
+// Much thanks to Corey in class for helping me out with this code, 
+// without him this would not have worked. Much thanks!!!!
+
 // Here is where LIRI gets asked what to do
 
 // my-tweets function
+
+
+
 function myTweets() {
     feed.get('statuses/user_timeline', userTwitter, function(error, tweets, response) {
         if (!error && response.statusCode == 200) {
@@ -60,31 +72,76 @@ function myTweets() {
     });
 } // end myTweets function
 
-// spotifyThis function
-function getSong(value) {
-    fs.appendFile('log.txt', ('---------- Log Entry ----------\n' + Date() + '\n' + process.argv + '\n \nUsed getSong()\n'));
+// spotify function
+
+function getSong(input) {
+    fs.appendFile("log.txt", ("-------- Log Entry --------\n" + Date() + "\n" + "User used getSong()\n"));
     var valType = "track";
-    var song = value;
-    // if null goes to the sign as default
-    if (value === null) {
-        value = "The Sign";
+    var song = input;
+
+    if (input == null) {
+        song = "The Sign";
     }
 
-    spotify.search({ type: "track", query: value }, function(err, data) {
-        // ('https://api.spotify.com/v1/search?q=' + value + '&type=track', function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            jsonBody = JSON.parse(body);
-            console.log(' ');
-            console.log('Artist: ' + jsonBody.tracks.items[0].artists[0].name);
-            console.log('Song: ' + jsonBody.tracks.items[0].name);
-            console.log('Preview Link: ' + jsonBody.tracks.items[0].preview_url);
-            console.log('Album: ' + jsonBody.tracks.items[0].album.name);
-            console.log(' ');
-            fs.appendFile('terminal.log', ('=============== LOG ENTRY BEGIN ===============\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS:\r\n$: ' + process.argv + '\r\n \r\nDATA OUTPUT:\r\n' + 'Artist: ' + jsonBody.tracks.items[0].artists[0].name + '\r\nSong: ' + jsonBody.tracks.items[0].name + '\r\nPreview Link: ' + jsonBody.tracks.items[0].preview_url + '\r\nAlbum: ' + jsonBody.tracks.items[0].album.name + '\r\n=============== LOG ENTRY END ===============\r\n \r\n'), function(err) {
-                if (err) throw err;
-            });
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else {
+            //console.log(JSON.stringify(data, null, 2));
+            var response = data.tracks.items[0];
+            var artist = response.artists[0].name;
+            var title = response.name;
+            var album = response.album.name;
+            var url = response.preview_url;
+            console.log(`${song}`,
+                `${artist}`,
+                `${title}`,
+                `${artist}`,
+                `${album}`,
+                `${url}`);
         }
+        // Do something with 'data' 
     });
-} // end spotifyThis function
+} // end getSong()
+
+// movie function
+function getMovie(input) {
+    fs.appendFile("log.txt", ("-------- Log Entry --------\n" + Date() + "\n" + "User used getMovie()\n"));
+    var movie = input;
+    if (movie === undefined) {
+        movie = "Mr. Nobody";
+    }
+    request(`http://www.omdbapi.com/?t=${movie}&y=&plot=short&apikey=40e9cece`, function(err, response, body) {
+        if (!err && response.statusCode === 200) {
+            var data = JSON.parse(body);
+            console.log(data);
+            var title = data.Title;
+            var year = data.Year;
+            var rated = data.Rated;
+            var ratingLength = data.Ratings.length;
+            console.log(ratingLength);
+            if (ratingLength === 0) {
+                var imdbRating = "Sorry this movie is yet to be rated."
+            } else {
+                var imdbRating = data.Ratings[0].Value;
+                console.log(data.Ratings[0]);
+            }
+            if (ratingLength > 1) {
+                var rottenRating = data.Ratings[1].Value;
+            } else {
+                var rottenRating = "We were too lazy to even watch this movie.";
+            }
+            var country = data.Country;
+            var language = data.Language;
+            var plot = data.Plot;
+            var actors = data.Actors;
+            var url = data.Website;
+            console.log(`Here is the ${movie} you searched for. It is rated: (${rated}) and was released in ${year}. It was released in ${country} in ${language} and featured ${actors}.
+                        Here is a quick plot of the film: ${plot}. The critics rated ${title}: IMDB: ${imdbRating} and Rotten Tomatoes: ${rottenRating}
+                        To learn more about this film you can visit: ${url}`);
+        } // end if()
+    }); // end request()
+} // end getMovie()
 
 // help me!!!
